1436c3139fa07640ef0bdafdeb1605b71e098746
Corrie Green <contact@corriejgreen.co.uk>
2015-10-19 18:34:37 +0100

    Update: New config style

--- config/config.php
+++ config/config.php
@@ -1,15 +1,8 @@
+
 <?php
-    //set off all error report
-	error_reporting(E_ALL);
-	//define constants database username and pass
-    define( "DB_DSN", "mysql:host=us-cdbr-azure-central-a.cloudapp.net;dbname=DB_WebAppDev" );
-    define( "DB_USERNAME", "b9e8fe48954215" );
-    define( "DB_PASSWORD", "934095f4" );
-	define( "CLS_PATH", "class" );
-	
-	//classes path
-	include_once( CLS_PATH . "/user.php" );
-	
-
+// Replace with your own
+$sUsername = 'b9e8fe48954215';
+$sPassword = '934095f4';
+$sHost = 'us-cdbr-azure-central-a.cloudapp.net';
+$sDb = 'DB_WebAppDev';
 ?>
-
--- index.php
+++ index.php
@@ -1,34 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Adventure Blog Demo Test</title>
-	<meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-<body>
-
 <?php
-			include_once("config/config.php");
-		
-								
-				$con = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD );
-				$sthandler = $con->prepare("SELECT * FROM users ORDER BY blogTime DESC");
-				$sthandler->execute();
 
-				while ($row = $sthandler->fetch(PDO::FETCH_ASSOC)){
-				
-							$postID = $row['postID'];
-							$title = $row['title'];
-							$post = $row['blogPost'];
-							$blogTime = $row['blogTime'];
-							
-							?>
-							
-							<h1><?php echo $title;?></h1>
-							<br>
-							<br>
-							<?php echo $blogTime;?>
-							<br>
-							<?php echo $post;?><br>
-</body>
-</html>
+require_once 'config.php';
+
+try {
+    $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+    $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+    $oResult = $oStmt->fetchAll();
+
+    foreach ($oResult as $aRow) {
+        print_r($aRow['data']);
+    }
+
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+?>
