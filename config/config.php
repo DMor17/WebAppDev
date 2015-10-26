cce7495ac630bad71fad11348e8c13882f88f8a0
Corrie Green <contact@corriejgreen.co.uk>
2015-10-26 14:24:41 +0000

    Update: Config pass
    pass added

--- config/config.php
+++ config/config.php
@@ -1,14 +1,13 @@
 <?php
     //set off all error report
 	error_reporting(E_ALL);
-	//define constants database username and pass
+	//defineatabase username and pass
     define( "DB_DSN", "mysql:host=localhost;dbname=DB_WebAppDev" );
-    define( "DB_USERNAME", "root" );
-    define( "DB_PASSWORD", "" );
+    define( "DB_USERNAME", "b9e8fe48954215" );
+    define( "DB_PASSWORD", "934095f4" );
 	define( "CLS_PATH", "class" );
 	
 	//classes path
 	include_once( CLS_PATH . "/user.php" );
 	
-
 ?>
