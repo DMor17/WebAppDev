38556ef24299126356788db4b941ab07307b202d
Corrie Green <contact@corriejgreen.co.uk>
2015-10-26 15:44:25 +0000

    Update: Changed the extension of the file
    Changed from inc to php

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
     //set off all error report
 	error_reporting(E_ALL);
 	//define database username and pass
-    define( "DB_DSN", "mysql:host=us-cdbr-azure-west-c.cloudapp.net;dbname=DB_WebApplicationDev" );
-    define( "DB_USERNAME", "b0d377dde65ed2" );
-    define( "DB_PASSWORD", "fe8a6525" );
+    define( "DB_DSN", "mysql:host=localhost;dbname=db_webappdev" );
+    define( "DB_USERNAME", "root" );
+    define( "DB_PASSWORD", "" );
 	define( "CLS_PATH", "class" );
 	//classes path
 	include_once( CLS_PATH . "/user.php" );
